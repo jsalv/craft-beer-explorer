c92e1003fd58be5846aff28b4485c9ce249c869d
jsalv <jsalvacion.17@outlook.com>
2024-02-28 20:50:48 -0500

    Added my project

---
+++ src/test/java/com/example/brewery/craftbeerexplorer/CraftBeerExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brewery.craftbeerexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftBeerExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
